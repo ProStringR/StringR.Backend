42446737261891106a6a31b3295b4b0301e36e4c
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-11-07 15:10:57 +0100

    Test endpoint function

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -17,6 +17,13 @@ namespace StringR.Backend.Controllers
             return new string[] { "value1", "value2" };
         }
 
+        //GET api/values/bosses
+        [HttpGet("/bosses")]
+        public ActionResult<IEnumerable<string>> GetBosses()
+        {
+            return new string[] {"Marcus", "Jaafar"};
+        }
+
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
