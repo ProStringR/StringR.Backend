69bd03d25d05eef8c2116cbb78496462aa7a0bd9
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-11-08 23:00:00 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace StringR.Backend
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>();
     }
 }
