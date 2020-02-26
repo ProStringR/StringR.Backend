d78dff62350e3e1c139f40fbee867f66249429bb
Jaafar92 <als-92@hotmail.com>
2020-02-26 12:42:43 +0100

    Fix return value from DB

--- StringR.Backend/DTO/RacketStringDto.cs
+++ StringR.Backend/DTO/RacketStringDto.cs
@@ -5,11 +5,11 @@ namespace StringR.Backend.DTO
         public int StringId { get; set; }
         public double Price { get; set; }
         public double LengthInStock { get; set; }
-        public int StringModel { get; set; }
-        public int StringType { get; set; }
-        public int StringBrand { get; set; }
+        public string StringModel { get; set; }
+        public string StringType { get; set; }
+        public string StringBrand { get; set; }
         public double Thickness { get; set; }
-        public int Purpose { get; set; }
-        public int Color { get; set; }
+        public string Purpose { get; set; }
+        public string Color { get; set; }
     }
 }
