7c49895ebf35a393a0dce9669fd0f82794ee9458
Jaafar92 <als-92@hotmail.com>
2020-02-26 09:38:08 +0100

    Show input data in Swagger for creating a customer

--- StringR.Backend/Models/Customer.cs
+++ StringR.Backend/Models/Customer.cs
@@ -2,15 +2,15 @@ namespace StringR.Backend.Models
 {
     public class Customer
     {
-        public string FirstName { get; }
-        public string LastName { get; }
-        public string Email { get; }
-        public string PhoneNumber { get; }
-        public string UserId { get; }
-        public string Password { get; }
-        public int PreferredStringTypeId { get; }
-        public double PreferredTensionVertical { get; }
-        public double PreferredTensionHorizontal { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        public int PreferredStringTypeId { get; set; }
+        public double PreferredTensionVertical { get; set; }
+        public double PreferredTensionHorizontal { get; set; }
 
         public Customer(string firstName, string lastName, string email, string phoneNumber, string userId, string password, int preferredStringTypeId, double preferredTensionVertical, double preferredTensionHorizontal)
         {
