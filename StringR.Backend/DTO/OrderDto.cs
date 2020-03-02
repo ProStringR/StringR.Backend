a521a03f37840bae17935110e0cf0c31f7f244da
Jaafar92 <als-92@hotmail.com>
2020-03-02 14:37:14 +0100

    Update OrderDto.cs

--- StringR.Backend/DTO/OrderDto.cs
+++ StringR.Backend/DTO/OrderDto.cs
@@ -10,13 +10,14 @@ namespace StringR.Backend.DTO
         public int OrderStatus { get; set; }
         public string Comment { get; set; }
         public double Price { get; set; }
+        public bool Paid { get; set; }
         public long DeliveryDate { get; set; }
         public double TensionVertical { get; set; }
         public double TensionHorizontal { get; set; }
         public OrderCustomerDto Customer { get; set; }
         public OrderStringerDto Stringer { get; set; }
         public RacketDto Racket { get; set; }
-        public OrderRacketStringDto OrderRacketString { get; set; }
+        public OrderRacketStringDto RacketString { get; set; }
 
         public OrderDto(DataRow row)
         {
@@ -24,6 +25,7 @@ namespace StringR.Backend.DTO
             OrderStatus = (int) row["orderStatus"];
             Comment = row["comment"].ToString();
             Price = (double) row["price"];
+            Paid = row["paid"].Equals(true);
             DeliveryDate = (long) row["deliveryDate"];
             TensionVertical = (double) row["tensionVertical"];
             TensionHorizontal = (double) row["tensionHorizontal"];
@@ -31,7 +33,7 @@ namespace StringR.Backend.DTO
             Customer = new OrderCustomerDto(row["customerFirstName"].ToString(), row["customerLastName"].ToString(), row["customerEmail"].ToString(), row["customerPhone"].ToString());
             Stringer = new OrderStringerDto(row["stringerFirstName"].ToString(), row["stringerLastName"].ToString(), row["stringerPhone"].ToString(), row["stringerEmail"].ToString());
             Racket = new RacketDto((int)row["racketId"], row["racketBrand"].ToString(), row["racketModel"].ToString(), (int) row["racketWeight"], (int) row["Racketmain"], (int) row["Racketcross"], (int) row["racketGripSize"]);
-            OrderRacketString = new OrderRacketStringDto((int) row["stringId"], row["stringBrand"].ToString(), row["stringModel"].ToString(), row["stringType"].ToString(), row["stringThickness"].ToString(), row["stringPurpose"].ToString(), row["stringColor"].ToString());
+            RacketString = new OrderRacketStringDto((int) row["stringId"], row["stringBrand"].ToString(), row["stringModel"].ToString(), row["stringType"].ToString(), row["stringThickness"].ToString(), row["stringPurpose"].ToString(), row["stringColor"].ToString());
         }
     }
 }
